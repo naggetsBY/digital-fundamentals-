d6cf7e02afb0c15627b9602e06895a00e22fca52
Kozlov Viktor Alexandrovich <vinderj32@gmail.com>
2025-12-07 22:01:40 +0300

    Create analyser3.sh

---
+++ scripts/analyser3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in $(ls $1)
+ do
+  for j in $(cat $1$i)
+   do
+    age=$(echo -e $j | cut -d ":" -f2) 
+    if [ $age -ge 18 ]
+     then teen=$((teen+1)) 
+     else nonteen=$((nonteen+1))
+    fi
+   done
+  done
+  
+echo "teen: $teen"
+echo "nonteen: $nonteen"
